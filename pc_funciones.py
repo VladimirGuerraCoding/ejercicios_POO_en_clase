e5b86380f29283aca93213f6d339a993e11572c3
Vladimir <vladimir.guerra.c@gmail.com>
2025-05-10 17:07:11 -0500

    poo anca 2-4

--- pc_funciones.py
+++ pc_funciones.py
@@ -1,17 +1,12 @@
-class Laptop:
-    def __init__(self, encender, apagar, actualizar):
-        self.encender = encender
-        self.apagar = apagar
-        self.actualizar = actualizar
+class Computadora:
+    def __init__(self, marca, memoria, almacenamiento, procesador):
+        self.marca = marca
+        self.memoria = memoria
+        self.almacenamiento = almacenamiento
+        self.procesador = procesador
+        print(f'El computador {self.marca} tiene memoria ram de {self.memoria} capacidad en disco de {self.almacenamiento} y un procesador {self.procesador} que garantiza su rendimiento.')
+        
+        
+computador1 = Computadora('Dell', '8GB', '2TB', 'Intel-Core-i3-5005U-CPU-2.00GHz')
     
-        self.encendido = False
-        
-    def encender(self):
-        if not self.encendido:
-            self.encendido = True
-            print(f'la computadora esta encendida')
-        else:
-            print('la computadora esta apagada')    
-        
-        
     
