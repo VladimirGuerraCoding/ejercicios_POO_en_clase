37b325b433effa10a601176279a276d178d43ad8
Jerson Chara bastidas <jersoncharabastidas@gmail.com>
2025-05-17 13:17:18 -0500

    Add files via upload

--- pc_funciones.py
+++ pc_funciones.py
@@ -1,12 +1,38 @@
-class Computadora:
-    def __init__(self, marca, memoria, almacenamiento, procesador):
-        self.marca = marca
-        self.memoria = memoria
-        self.almacenamiento = almacenamiento
-        self.procesador = procesador
-        print(f'El computador {self.marca} tiene memoria ram de {self.memoria} capacidad en disco de {self.almacenamiento} y un procesador {self.procesador} que garantiza su rendimiento.')
-        
-        
-computador1 = Computadora('Dell', '8GB', '2TB', 'Intel-Core-i3-5005U-CPU-2.00GHz')
+class laptop:
+    def __init__(self, encender, apagar, actualizar):
+        self.encender = encender
+        self.apagar = apagar
+        self.actualizar =actualizar
+
+    def encender_pc(self):
+        if not self.encender:
+            self.encender = True
+            print(f"la computadora esta encendida")
+        else:
+            print(f"la computadora ya esta encendida ")
+    def apagar(self):
+
+        if self.encender:
+            self.encender = False
+            print(f"la computadora esta apagada")
     
+    def actualizar_ram(self, nueva_ram):
+        if not self.encendida:
+            print(
+                "Por favor, encienda la computadora antes de actualizar la RAM para evitar daños  o bugs en la ram xd."
+            )
+            return
+        if nueva_ram > self.ram:
+            print(f"Actualizando RAM de {self.ram}GB a {nueva_ram}GB.")
+            self.ram = nueva_ram
+        else:
+            print("La nueva RAM debe ser mayor que la actual.")
+
+
+mi_computadora = laptop ("Dell", "Inspiron", 8)
+mi_computadora.encender()
+mi_computadora.actualizar_ram(16)
+mi_computadora.apagar()
+        
+                      
     
